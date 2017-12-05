03a358e10aba2e0980c476069ea890d2d096ca9d
CodeZaru <teamsullivank@gmail.com>
2017-12-05 13:01:49 -0500

    tweaks

---
+++ routes/favorites-routes.js
@@ -0,0 +1,49 @@
+let db = require('../models');
+
+module.exports = function(app) {
+    // gets all da Artists on the menu
+    app.get('/', (req, res) => {
+        db.Artist.findAll().then(artists => {
+            db.Selection.findAll({
+                'where': {
+                    'selected': 0
+                },
+                'include': [ db.Artist ]
+            }).then(selections => {
+                res.render('index', { artists, selections });
+            });
+        });
+    });
+
+    // posts a new artist to selections
+    app.post('/selection/:artistId', (req, res) => {
+        db.Selection.create({
+            'selected': 0,
+            'ArtistId': req.params.artistId
+        }).then(() => res.redirect('/'));
+    });
+
+    // posts a new artist to the menu
+    app.post('/new', (req, res) => {
+        db.Artist.create(req.body).then(() => res.redirect('/'));
+    });
+
+    // removes a artist from the favorites panel - OR - removes a artist from the selection
+    app.delete('/:table/:id', function(req, res) {
+        let table = req.params.table;
+
+        if (table === 'selections') {
+            db.Selection.destroy({
+                'where': {
+                    'id': req.params.id
+                }
+            }).then(() => res.redirect('/playlist'));
+        } else if (table === 'artists') {
+            db.Artist.destroy({
+                'where': {
+                    'id': req.params.id
+                }
+            }).then(() => res.redirect('/'));
+        }
+    });
+}
